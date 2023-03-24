0f72279bf62b574f95b5acd4464fda09d25ecd8a
Adriath <adrianarjonabravo@gmail.com>
2023-03-24 19:04:33 +0100

    Creación de la clase JuegoMosca para empezqar el ejercicio 2.

---
+++ src/ejercicio2/maestre/JuegoMosca.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2.maestre;
+
+/**
+ * Operaciones con arrays unidimensionales.
+ * Vamos a realizar un juego en el que se debe atrapar una mosca que está alojada 
+ * en una posición aleatoria de un array.
+ * 
+ * @author Adrián Arjona
+ */
+public class JuegoMosca {
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 7: aplicación de las estructuras de almacenamiento.
+    */
+    
+    
+}
