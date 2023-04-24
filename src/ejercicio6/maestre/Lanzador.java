03c3d1872acfa1a421dab26cb53241f538825cad
Adriath <adrianarjonabravo@gmail.com>
2023-04-24 16:12:02 +0200

    Creación del paquete y clases del ejercicio 6.

---
+++ src/ejercicio6/maestre/Lanzador.java
@@ -0,0 +1,24 @@
+
+package ejercicio6.maestre;
+
+/**
+ * Lanzador del programa MasterMind.
+ * 
+ * @author Adrián Arjona
+ * @version 1.0 Marzo 2023
+ */
+public class Lanzador {
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 7: aplicación de las estructuras de almacenamiento.
+    */
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
---
+++ src/ejercicio6/maestre/MasterMind.java
@@ -0,0 +1,18 @@
+
+package ejercicio6.maestre;
+
+/**
+ * EJERCICIO 6
+ * Juego del MasterMind.
+ * 
+ * @author Adrián Arjona
+ * @version 1.0 Marzo 2023
+ */
+public class MasterMind {
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 7: aplicación de las estructuras de almacenamiento.
+    */
+    
+    
+}
