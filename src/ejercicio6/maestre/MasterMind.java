6f3865d0442a78652ef4d1bcf05bf3f8b91637ba
Adriath <adrianarjonabravo@gmail.com>
2023-04-24 19:33:56 +0200

    Ya funciona el constructor y la generación de los valores en el primer array.

--- src/ejercicio6/maestre/MasterMind.java
+++ src/ejercicio6/maestre/MasterMind.java
@@ -1,6 +1,8 @@
 
 package ejercicio6.maestre;
 
+import utilidades.Utilidades;
+
 /**
  * EJERCICIO 6
  * Juego del MasterMind.
@@ -18,22 +20,51 @@ public class MasterMind {
     // ---------------- DECLARACIÓN DE VARIABLES -----------------
     
     private int array[] ;
+    private int arrayUsuario[] ;
     
     // ------------------ MÉTODOS -----------------------------
     
-        // CONSTRUCTOR
+        // ------ CONSTRUCTOR ---------
     
-        public MasterMind(int[] array){ // CONSTRUCTOR CON PARÁMETROS
+        public MasterMind(){ // CONSTRUCTOR CON PARÁMETROS
         
-            this.array = array ;
+            this.array = new int[3];
+            this.arrayUsuario = new int[3] ;
         }
     
-        // GETTERS & SETTERS
+        // ------- GETTERS & SETTERS ---------
 
         public int[] getArray() { // GETTER DE ARRAY
             
             return array;
         }
+        
+        public int[] getArrayUsuario() { // GETTER DE ARRAY USUARIO
+            
+            return array;
+        }
     
         
+        // ------ PERSONALIZADOS -------
+        
+        public void generaValoresAleatorios(int[] array){
+            //MÉTODO QUE GENERA TRES NÚMEROS ALEATORIOS Y LOS GUARDA EN EL ARRAY
+            
+            array[0] = Utilidades.numeroAleatorioDesdeoCero(10) ; // Genera número del 0 al 9 y lo guarda en la primera posición.
+            array[1] = Utilidades.numeroAleatorioDesdeoCero(10) ; // Genera número del 0 al 9 y lo guarda en la segunda posición.
+            array[2] = Utilidades.numeroAleatorioDesdeoCero(10) ; // Genera número del 0 al 9 y lo guarda en la tercera posición.
+        }
+        public static void main(String[] args) {
+        int array[] ;
+        
+        MasterMind partida1 = new MasterMind() ;
+        
+        array = partida1.getArray() ;
+        
+        partida1.generaValoresAleatorios(array);
+        
+            for (int i : array) {
+                System.out.println(i);
+            }
+    }
 }
